35124a750bc22ee335a1339224c7a6e7e9c5cfef
shourjoac <131592449+shourjoac@users.noreply.github.com>
2025-05-04 21:48:44 -0500

    monitoring added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const multer = require('multer');
 const poRoutes = require('./routes/po');
+const metricsRouter = require('./metrics');
+
 require('dotenv').config();
 const cors = require('cors');
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/po', poRoutes);
+app.use(metricsRouter);
+
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
